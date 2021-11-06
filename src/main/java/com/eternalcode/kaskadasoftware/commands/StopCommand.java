bd9d9dfd25e7da0c7c208b435f0823ca3786eb06
vLucky <65517973+vLuckyyy@users.noreply.github.com>
2021-11-06 19:00:49 +0100

    Create StopCommand.java

---
+++ src/main/java/com/eternalcode/kaskadasoftware/commands/StopCommand.java
@@ -0,0 +1,19 @@
+package com.eternalcode.kaskadasoftware.commands;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.CommandSender;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.CommandContext;
+import org.jetbrains.annotations.NotNull;
+
+public class StopCommand extends Command {
+
+    public StopCommand() {
+        super("shutdown", "stop");
+        addSyntax(this::execute);
+    }
+
+    private void execute(@NotNull CommandSender commandSender, @NotNull CommandContext commandContext) {
+        MinecraftServer.stopCleanly();
+    }
+}
